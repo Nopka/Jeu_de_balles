27dd0446a69106de065f6ff407245ae6f122ac9f
Hugo ROUSSILLON <39841772+Nopka@users.noreply.github.com>
2019-03-12 08:50:47 +0100

    Delete fonction.js

--- fonction.js
+++
@@ -1,9 +0,0 @@
-function PopUp_ON(){
-     document.getElementById('EcranJeu').style.display = 'block';
-}
-
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
-function jeu(){
-
-}
